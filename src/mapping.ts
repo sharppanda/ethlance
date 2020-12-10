31a5633ad114e8c123dfe3968d9d1c9b955c12e7
Martin Paulicek <martin@agile-ventures.com>
2020-12-10 17:44:57 +0100

    Add updating

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,7 +2,12 @@ import {  SetJobCall } from '../generated/Ethlance/Jobs'
 import { Job } from '../generated/schema'
 
 export function handleSetJob(event: SetJobCall): void {
-  let job = new Job(event.inputs.jobId.toHex())
+  let id = event.inputs.jobId.toHex()
+  let job = Job.load(id)
+  if (job == null) {
+    job = new Job(id)
+  }
+
   job.title = event.inputs.title
   job.description = event.inputs.description
   job.save()
